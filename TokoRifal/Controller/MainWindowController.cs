aae629ad2a3aa792a1f7cf8dd79ea65a0f9bb590
rifalfawzi <67996351+rifalfawzi@users.noreply.github.com>
2021-01-12 20:40:39 +0700

    Add files via upload

---
+++ TokoRifal/Controller/MainWindowController.cs
@@ -0,0 +1,46 @@
+﻿using TokoRifal.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokoRifal.Controller
+{
+  class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja, Payment payment)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void addDiskon(Diskon diskon)
+        {
+
+            this.keranjangBelanja.addDiskon(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.keranjangBelanja.getDiskon();
+        }
+
+
+    }
+}
---
+++ TokoRifal/Controller/PromoController.cs
@@ -0,0 +1,27 @@
+﻿using TokoRifal.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokoRifal.Controller
+{
+    public class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
